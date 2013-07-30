d04a2b251fc44b98e5daa2065e76798c863d0b0d
mgpyone <mgpyone@mysteryzillion.com>
2013-07-30 18:34:05 +0630

    Added for loop

---
+++ constants.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+import "math"
+
+// const declares a constant value
+const s string = "constant"
+
+func main() {
+  fmt.Println(s)
+
+  // A const state can appear anywhere
+  const n = 5000000
+
+  // arithmetic with arbitrary precision
+  const d = 3e20 / n
+  fmt.Println(d)
+
+  // explict cast
+  fmt.Println(int64(d))
+
+  // math package
+  fmt.Println(math.Sin(n))
+
+}
---
+++ for.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+  i :=1
+
+  // single condition loop
+  for i <= 3 {
+    fmt.Println(i)
+    i = i + 1
+  }
+
+  fmt.Println("xxxxxxxxxxxx")
+  // class initial / condiiton / after for loop
+  for j := 7; j <= 9; j++ {
+    fmt.Println(j)
+  }
+
+  fmt.Println("xxxxxxxxxxxx")
+
+  // for without a condition will loop repeatedly until you 'break'
+  for {
+    fmt.Println("loop")
+    break
+  }
+}
